83f64304813b4c1837474b88ccf110d5e5de1775
charlesChaseIII <charleschase14@gmail.com>
2022-02-18 01:05:33 -0500

    Thread Practice New notes and project revisions
    Created a new main driver notepad file and finished implementing thread in thread application.

--- Java Projects/JPMorganInterviewPractice/src/rev/com/prac/ThreadApplication.java
+++ Java Projects/JPMorganInterviewPractice/src/rev/com/prac/ThreadApplication.java
@@ -2,12 +2,16 @@ package rev.com.prac;
 import rev.com.prac.ThreadPractice;
 
 
-public class ThreadApplication {
+public class ThreadApplication extends Thread{
 	
-	//ThreadPractice lastInd;
+	//public ThreadPractice lastInd;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ThreadApplication thread = new ThreadApplication();
+		
+		thread.start();
 		
 		ThreadPractice lastEle = new ThreadPractice();
 		
@@ -15,20 +19,18 @@ public class ThreadApplication {
 		
 		System.out.println(lastEle.getLastElement(nums));
 		
-		System.out.println(runnable());
+		
 	}
 	
-	public static Runnable runnable() {
+	public void run() {
 		
 		ThreadPractice lastInd = new ThreadPractice();
 		
-        int[] difNums = {12,25,68,70,100};
+        int[] difNums = {12,25,68,70,200};
 		
 		System.out.println(lastInd.getLastElement(difNums));
 		
-		Runnable run = null;
 		
-		return run;
 	}
 
 }
